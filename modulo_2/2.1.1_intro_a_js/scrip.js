c905534eb2b460d579d4cd3247e5f4ae8b462b43
fabian <fedurans@live.com>
2023-06-24 10:56:26 -0500

    avance modulo 2.1

---
+++ modulo_2/2.1.1_intro_a_js/scrip.js
@@ -0,0 +1,11 @@
+
+console.log("hola mundo");
+document.write("hola.mundo x2")
+alert("hola mundo x3");
+var mensaje = "utilizando cadena de texto"
+document.write(mensaje)
+
+// var 
+// let = cuando se quiere actualizar en algun momento 
+// const= cuando no tiene que mantener la estructura 
+
